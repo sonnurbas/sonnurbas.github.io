0316068975c708e47b4282eff7aff17d551de0ce
sonnurbas <71406222+sonnurbas@users.noreply.github.com>
2026-05-13 14:14:09 +0000

    Deploying to gh-pages from @ sonnurbas/sonnurbas.github.io@740b757e8b64028ac3f307e6f833ac94a6a5231d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,14 +3,14 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-about",
-          title: "about",
+  },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
